934dbb52f56abcbd757034bdadb3fc907f731713
Timm Knape <timm.knape@mechatronic.de>
2024-01-09 16:16:24 +0100

    use shared pointer for type in variable

--- include/decl/variable.h
+++ include/decl/variable.h
@@ -1,20 +1,23 @@
 #pragma once
 
+#include <utility>
+
 #include "decl/type.h"
 
 namespace decl {
     class Variable : public Declaration {
     public:
         Variable(
-                Declaration *enclosing_declaration, llvm::SMLoc loc,
-                std::string name, Type *type
+            Declaration *enclosing_declaration, llvm::SMLoc loc,
+            std::string name, std::shared_ptr<type::Type> type
         ) :
-                Declaration(enclosing_declaration, loc, std::move(name)),
-                type_{type} {}
+            Declaration(enclosing_declaration, loc, std::move(name)),
+            type_ { std::move(type) }
+        {}
 
-        Type* type() { return type_; }
+        [[nodiscard]] std::shared_ptr<type::Type> type() const { return type_; }
 
     private:
-        Type* type_;
+        std::shared_ptr<type::Type> type_;
     };
 }
