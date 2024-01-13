25351a775832d0a0c7c7b4f052c947a61accb48c
Timm Knape <timm@knp.de>
2024-01-13 17:07:48 +0100

    create WHILE statement

--- tools/tests/parser-tests/while-statement-tests.cpp
+++ tools/tests/parser-tests/while-statement-tests.cpp
@@ -1,24 +1,30 @@
 #include "parser-tests.h"
 #include "decl/variable.h"
+#include "stmt/while.h"
 
 using While_Statement_Runner = Parser_Value_Runner<
-	sema::While_Statement, &Parser::parse_while_statement
+	sema::Statement, &Parser::parse_while_statement
 >;
 
 TEST(While_Statement_Tests, empty) {
 	Scope scope;
-	sema::While_Statement while_statement { scope };
-	While_Statement_Runner test1 { "", while_statement, true };
+	sema::Statement statement { scope };
+	While_Statement_Runner test1 { "", statement, true };
 }
 
 TEST(While_Statement_Tests, simple) {
 	Scope scope;
 	scope.insert("a", std::make_shared<decl::Variable>(nullptr));
 	scope.insert("INC", std::make_shared<decl::Variable>(nullptr));
-	sema::While_Statement while_statement { scope };
-	While_Statement_Runner test1 {
-		"WHILE a < 3 DO INC(a) END", while_statement
-	};
+	sema::Statement statement { scope };
+	While_Statement_Runner test1 { "WHILE a < 3 DO INC(a) END", statement };
+	auto while_statement { std::dynamic_pointer_cast<stmt::While>(
+		statement.statement
+	)};
+	EXPECT_NE(while_statement, nullptr);
+	if (while_statement) {
+		EXPECT_EQ(while_statement->entries.size(), 1);
+	}
 }
 
 TEST(While_Statement_Tests, with_elsif) {
@@ -27,32 +33,35 @@ TEST(While_Statement_Tests, with_elsif) {
 	scope.insert("b", std::make_shared<decl::Variable>(nullptr));
 	scope.insert("c", std::make_shared<decl::Variable>(nullptr));
 	scope.insert("INC", std::make_shared<decl::Variable>(nullptr));
-	sema::While_Statement while_statement { scope };
+	sema::Statement statement { scope };
 	While_Statement_Runner test1 {
 		"WHILE a < 3 DO INC(a) ELSIF b < 3 DO INC(b) ELSIF c < 3 DO INC(c) END",
-		while_statement
+		statement
 	};
+	auto while_statement { std::dynamic_pointer_cast<stmt::While>(
+		statement.statement
+	)};
+	EXPECT_NE(while_statement, nullptr);
+	if (while_statement) {
+		EXPECT_EQ(while_statement->entries.size(), 3);
+	}
 }
 
 TEST(While_Statement_Tests, wrong) {
 	Scope scope;
 	scope.insert("a", std::make_shared<decl::Variable>(nullptr));
 	scope.insert("INC", std::make_shared<decl::Variable>(nullptr));
-	sema::While_Statement while_statement { scope };
-	While_Statement_Runner test1 { "WHILE DO", while_statement, true, true };
+	sema::Statement statement { scope };
+	While_Statement_Runner test1 { "WHILE DO", statement, true, true };
 
-	while_statement.clear();
-	While_Statement_Runner test2 {
-		"WHILE a < 3 DO INC(a)", while_statement, true
-	};
+	statement.clear();
+	While_Statement_Runner test2 { "WHILE a < 3 DO INC(a)", statement, true };
 
-	while_statement.clear();
-	While_Statement_Runner test3 {
-		"WHILE a ELSIF", while_statement, true, true
-	};
+	statement.clear();
+	While_Statement_Runner test3 { "WHILE a ELSIF", statement, true, true };
 
-	while_statement.clear();
+	statement.clear();
 	While_Statement_Runner test4 {
-		"WHILE a < 3 DO ELSIF END", while_statement, true, true
+		"WHILE a < 3 DO ELSIF END", statement, true, true
 	};
 }
